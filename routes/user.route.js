52f5fa896aa542b7cedd5c37e83dbbf0566186e9
Adhil PK <adhil.pk@zoft.care>
2024-03-07 20:57:34 +0530

    started user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,3 +1,25 @@
 const express = require("express")
 const router = express.Router()
+router.get('/login', (req, res) => {
+    res.render('user/login', { layout: 'layouts/main' });
+  });
+router.get('/register', (req, res) => {
+    res.render('user/register', { layout: 'layouts/main' });
+  });
+  router.get('/home', (req, res) => {
+    res.render('user/home', { layout: 'layouts/main' });
+  });
+  router.get('/product', (req, res) => {
+    res.render('user/product-list', { layout: 'layouts/main' });
+  });
+   router.get('/productDetail', (req, res) => {
+    res.render('user/product-detail', { layout: 'layouts/main' });
+  });
+  router.get('/profile', (req, res) => {
+    res.render('user/profile', { layout: 'layouts/main' });
+  });
+  router.get('/cart', (req, res) => {
+    res.render('user/cart', { layout: 'layouts/main' });
+  });
+  
 module.exports = router
